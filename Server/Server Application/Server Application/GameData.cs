9001d33494f5a4aec795049c52ab83612ac629d8
Michael Dimchuk <michaeldimchuk@gmail.com>
2014-09-25 13:30:50 -0700

    Setting up the server classes.

--- Server/Server Application/Server Application/GameData.cs
+++ Server/Server Application/Server Application/GameData.cs
@@ -9,11 +9,54 @@ namespace Server_Application
     class GameData
     {
         int gameroom;
-        int username;
-        byte health;
+        string ipaddress;
+        short port;
         short xposition;
         short yposition;
         float angle;
+        byte health;
         //projectiles fired, direction, velocity
+
+        public int GameRoom
+        {
+            set { gameroom = value; }
+            get { return gameroom; }
+        }
+
+        public string IP
+        {
+            set { ipaddress = value; }
+            get { return ipaddress; }
+        }
+
+        public short Port
+        {
+            set { port = value; }
+            get { return port; }
+        }
+
+        public short XPosition
+        {
+            set { xposition = value; }
+            get { return xposition; }
+        }
+
+        public short YPosition
+        {
+            set { yposition = value; }
+            get { return yposition; }
+        }
+
+        public float Angle
+        {
+            set { angle = value; }
+            get { return angle; }
+        }
+
+        public byte Health
+        {
+            set { health = value; }
+            get { return health; }
+        }
     }
 }
