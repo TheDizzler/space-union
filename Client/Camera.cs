34d3a8e59ec549eda1ab4abbe60cc743608cd388
TheDizzler <tgillon.music@gmail.com>
2014-09-25 20:35:39 -0700

    attempting to implement camera

---
+++ Client/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceUnion {
+	class Camera {
+
+		public float zoom;
+		/// <summary>
+		/// Camera View Matrix
+		/// </summary>
+		public Matrix transform;
+		/// <summary>
+		/// Inverse of View Matrix, used to get objects screen coordinates
+		/// form its object coordinates
+		/// </summary>
+		public Matrix inverseTransform;
+		protected Vector2 position;
+		protected float rotation;
+		Viewport viewport;
+		MouseState mouseState;
+		KeyboardState keyState;
+		int scroll;
+
+		
+	}
+}
