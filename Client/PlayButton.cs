d0fc9a94f2dc203f33837b4869f7ead70635620d
TheDizzler <tgillon.music@gmail.com>
2014-09-28 00:27:17 -0700

    refactored playbutton to inherit sprite

--- Client/PlayButton.cs
+++ Client/PlayButton.cs
@@ -5,20 +5,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SpaceUnion.Tools;
+
 
 namespace SpaceUnion {
-	class PlayButton {
-		Texture2D texture;
-		Vector2 position;
-		Rectangle buttonRectangle;
+	class PlayButton : Sprite {
 
+		Rectangle buttonRectangle;
 		public Vector2 size;
 
-		public PlayButton(Texture2D newTexture, GraphicsDevice graphics) {
+		public bool isClicked = false;
+
+		public PlayButton(Texture2D newTexture, GraphicsDevice graphics)
+			: base(newTexture, Vector2.Zero) {
+
 			texture = newTexture;
 			size = new Vector2(300, 150);
 		}
-		public bool isClicked = false;
+		
 
 		public void Update(MouseState mouse) {
 			buttonRectangle = new Rectangle((int) position.X, (int) position.Y, (int) size.X, (int) size.Y);
