62a2c0b46dad819a7fb1ec7e884a5b0ba8c74f67
Konstantin Boyarinov <konstabro@gmail.com>
2014-10-24 12:42:41 -0700

    Table Class implemented

--- Client/Controllers/LobbyBrowser.cs
+++ Client/Controllers/LobbyBrowser.cs
@@ -7,27 +7,43 @@ using System.Linq;
 using System.Text;
 using SpaceUnion.Ships;
 using SpaceUnion.Tools;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using SpaceUnion.Controllers;
+using System.Threading;
 ///Created by Matthew Baldock
 namespace SpaceUnion.Controllers
 {
     class LobbyBrowser
     {
+        static int MAX_AMOUNT_SERVERS = 1024;
         BaseButton nextlayerButton;
         CustomGUI banner;
         BaseButton lastlayerButton;
         Game1 game;
+
+        int rowRectOriginY = 125;
+        int rowRectOriginX = 100;
+        int rowRectSizeY = 15;
+        int rowRectSizeX = 500;
+
+        Table LobbyBrowserTable;
         /// <summary>
         /// Creates Buttons to other GameStates
         /// </summary>
         /// <param name="game"></param>
         public LobbyBrowser(Game1 game)
         {
+
             this.game = game;
             nextlayerButton = new BaseButton(Game1.Assets.gamelobby) { height = 100, width = 300 };
             banner = new CustomGUI(game, "Lobby browser");
             lastlayerButton = new BaseButton(Game1.Assets.lobbyoptions) { height = 100, width = 300 };
             lastlayerButton.setPosition(new Vector2(0, game.getScreenHeight() - 300));
             nextlayerButton.setPosition(new Vector2(0, game.getScreenHeight() - 100));
+
+            LobbyBrowserTable = new Table(6, new string[6] { "Lobby Name", "Game Type", "Host Name", "Players", "Max. Players", "Ping" }, rowRectOriginX, rowRectOriginY, rowRectSizeY, 17, 100);
+            
           
         }
         /// <summary>
@@ -35,6 +51,23 @@ namespace SpaceUnion.Controllers
         /// </summary>
         public void update()
         {
+            
+
+            var newState = Keyboard.GetState();
+
+            if (newState.IsKeyDown(Keys.D1))
+            {
+                LobbyBrowserTable.CreateNewRow(new string[6] {"CST", "Team Battle", "Konstantin", "3", "6", "100"});
+                Thread.Sleep(100);
+            }
+             
+
+            if (newState.IsKeyDown(Keys.D0))
+            {
+                LobbyBrowserTable.RemoveLastRow();
+                Thread.Sleep(100);
+            }
+            
             MouseState mouseState = Mouse.GetState();
             nextlayerButton.update(mouseState);
             lastlayerButton.update(mouseState);
@@ -58,10 +91,13 @@ namespace SpaceUnion.Controllers
         public void draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
+            LobbyBrowserTable.drawTable(spriteBatch);
             nextlayerButton.draw(spriteBatch);
             banner.draw(spriteBatch);
             lastlayerButton.draw(spriteBatch);
             spriteBatch.End();
         }
+
+       
     }
 }
---
+++ Client/Controllers/Table.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpaceUnion.Ships;
+using SpaceUnion.Tools;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using SpaceUnion.Controllers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceUnion.Controllers
+{
+    class Table
+    {
+        static int MAX_AMOUNT = 1024;
+        Texture2D columnLineTexture;
+        Rectangle[] rowLine = new Rectangle[MAX_AMOUNT + 1];
+        Rectangle[,] columnLine;
+        int NumberOfColumns;
+        Texture2D rowLineTexture;
+        Texture2D rowRectangleTexture;
+        Rectangle[] rowRectangle = new Rectangle[MAX_AMOUNT];
+        int rows;
+        int RowRectOriginY = 125;
+        int RowRectOriginX = 100;
+        int RowRectSizeY = 15;
+        int RowRectSizeX = 500;
+        Rectangle browserRectangle;
+        int Column1TextX, Column1TextY;
+        string[] ColumnNames;
+        string[,] RowArray = new string[MAX_AMOUNT, MAX_AMOUNT];
+        SpriteFont font;
+        int ColumnWidth;
+        
+        public Table(int column, string[] columnNames, int rowRectOriginX, int rowRectOriginY, int rowRectSizeY, int RowsBeforeScroll, int columnWidth)
+        {
+            font = Game1.Assets.font;
+            NumberOfColumns = column;
+            ColumnNames = new string[column];
+            ColumnNames = columnNames;
+            
+            columnLine = new Rectangle[MAX_AMOUNT, NumberOfColumns+1];
+            RowRectOriginY = rowRectOriginY;
+            RowRectOriginX = rowRectOriginX;
+            RowRectSizeY = rowRectSizeY;
+            Column1TextX = RowRectOriginX + 5;
+            Column1TextY = RowRectOriginY + 3;
+            RowRectSizeX = column * columnWidth;
+            
+            rowRectangleTexture = Game1.Assets.guiRectangle;
+            columnLineTexture = Game1.Assets.guiRectangle;
+            rowLineTexture = Game1.Assets.guiRectangle;
+            rowRectangle[0] = new Rectangle(rowRectOriginX, rowRectOriginY, RowRectSizeX, rowRectSizeY);
+            browserRectangle = new Rectangle(rowRectOriginX, rowRectOriginY, RowRectSizeX, rowRectSizeY * RowsBeforeScroll);
+            rowLine[0] = new Rectangle(rowRectOriginX, rowRectOriginY, RowRectSizeX, 1);
+            ColumnWidth =columnWidth;
+            rows = 1;
+            
+        }
+
+        public void drawTable(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(rowRectangleTexture, browserRectangle, Color.White);
+            for (int j = 0; j < rows; j++)
+            {
+                
+                rowLine[j] = new Rectangle(RowRectOriginX, RowRectOriginY + (j * RowRectSizeY), RowRectSizeX, 1);
+                rowLine[j + 1] = new Rectangle(RowRectOriginX, RowRectOriginY + ((j + 1) * RowRectSizeY), RowRectSizeX, 1);
+                for (int k = 100, c = 0;  c < NumberOfColumns; c++, k += ColumnWidth)
+                {
+                    columnLine[j, c] = new Rectangle(k, RowRectOriginY + (j * RowRectSizeY), 1, RowRectSizeY);
+                }
+                spriteBatch.Draw(rowRectangleTexture, rowRectangle[j], Color.White);
+                spriteBatch.Draw(rowLineTexture, rowLine[j], Color.Black);
+                spriteBatch.Draw(rowLineTexture, rowLine[j + 1], Color.Black);
+                for (int i = 0; i < NumberOfColumns; i++)
+                {
+                    spriteBatch.Draw(columnLineTexture, columnLine[j, i], Color.Black);
+                }
+                if (j == 0)
+                {
+                    for (int c = 0, k = 0; c < NumberOfColumns; c++, k += ColumnWidth)
+                    {
+                        spriteBatch.DrawString(font, ColumnNames[c],
+                            new Vector2(Column1TextX + k, Column1TextY), Color.Black, 0.0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0.5f);
+                    }
+                }
+                else
+                {
+                    for (int i = 0, k = 0; i < NumberOfColumns; i++, k += ColumnWidth)
+                    {
+
+                        spriteBatch.DrawString(font, RowArray[j, i],
+                            new Vector2(Column1TextX + k, Column1TextY + j * RowRectSizeY), Color.Black, 0.0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0.5f);
+                    }
+                }
+
+            }
+
+        }
+
+        public void CreateNewRow(string[] rowArray)
+        {
+                
+                for (int j = 0; j < NumberOfColumns; j++)
+                {
+                    RowArray[rows,j] = rowArray[j]; 
+
+                }
+
+
+            
+            rows++;
+
+
+
+        }
+
+        public void RemoveLastRow()
+        {
+            if (rows != 1)
+            {
+                rows--;
+
+            }
+
+        }
+    }
+}
