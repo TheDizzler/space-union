f4d37ee5a941fb01279c5b665ed4a8e8488a2ed2
Troy Carefoot <twcarefoot@gmail.com>
2014-10-29 23:26:04 -0700

    Creating Menus

---
+++ Menus/SpaceMenus/SpaceMenus/Game1.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Nuclex.UserInterface.Controls.Desktop;
+using Nuclex.UserInterface.Visuals.Flat;
+using Nuclex.Input;
+using Nuclex.UserInterface;
+
+
+namespace SpaceMenus
+{
+
+    /// <summary>Demonstrates the capabilities of the Nuclex UserInterface library</summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        /// <summary>Initializes and manages the graphics device</summary>
+        public GraphicsDeviceManager graphics_device_manager;
+        /// <summary>Manages the graphical user interface</summary>
+        public GuiManager gui_manager;
+        /// <summary>Manages input devices for the game</summary>
+        public InputManager input_manager;
+        /// <summary>Initializes a new instance of the user interface demo</summary>
+        public LoginMenu login_menu;
+        public MainMenu  main_menu;
+        public Screen mainScreen;
+
+        /// <summary>
+        /// Game State Enum to track game states
+        /// </summary>
+        enum GameState
+        {
+            Login,
+            MainMenu,
+            Playing,
+            Options,
+            TeamBattle,
+        }
+
+        GameState currentGameState = GameState.Login;
+
+
+        public Game1()
+        {
+            graphics_device_manager = new GraphicsDeviceManager(this);
+            input_manager = new InputManager(Services, Window.Handle);
+            gui_manager = new GuiManager(Services);
+            // Automatically query the input devices once per update
+            Components.Add(this.input_manager);
+            gui_manager.DrawOrder = 1000;
+
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Viewport viewport = GraphicsDevice.Viewport;
+            mainScreen = new Screen(viewport.Width, viewport.Height);
+            gui_manager.Screen = mainScreen;
+            gui_manager.Initialize();
+            //Screen Margins
+            mainScreen.Desktop.Bounds = new UniRectangle(
+              new UniScalar(0.1f, 0.0f), new UniScalar(0.1f, 0.0f), // x and y = 10%
+              new UniScalar(0.8f, 0.0f), new UniScalar(0.8f, 0.0f) // width and height = 80%
+            );
+
+            login_menu = new LoginMenu(this); //Users must login to play online
+        }
+
+        protected override void LoadContent()
+        {
+        }
+
+        protected override void UnloadContent()
+        {
+            Content.Unload();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //Allows game to exit.
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+				|| Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+            //Update the current state
+            switch (currentGameState)
+            {
+                case GameState.Login:
+                    login_menu.Update(gameTime);
+                    break;
+                case GameState.MainMenu:
+                    main_menu.Update(gameTime);
+                    break;
+                default:
+                    break;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>This is called when the game should draw itself.</summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightSeaGreen);
+
+            //Draw the current state
+            switch (currentGameState)
+            {
+                case GameState.Login:
+                    login_menu.DrawMenu(gameTime);
+                    break;
+                case GameState.MainMenu:
+                    main_menu.DrawMenu(gameTime);
+                    break;
+                default:
+                    break;
+            }
+            base.Draw(gameTime);
+        }
+
+        public void EnterMainMenu()
+        {
+            currentGameState = GameState.MainMenu;
+            main_menu = new MainMenu(this);
+        }
+
+        public void EnterLoginMenu()
+        {
+            currentGameState = GameState.Login;
+            login_menu = new LoginMenu(this);
+        }
+    }
+
+}
---
+++ Menus/SpaceMenus/SpaceMenus/LoginMenu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Nuclex.UserInterface.Controls.Desktop;
+using Nuclex.Input;
+using Nuclex.UserInterface;
+using Nuclex.UserInterface.Controls;
+
+namespace SpaceMenus
+{
+    public class LoginMenu
+    {
+        private Game1 game;
+
+        public LoginMenu(Game1 game)
+        {
+            this.game = game;
+            game.mainScreen.Desktop.Children.Clear(); //Clear the gui
+            CreateMenuControls(game.mainScreen);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void DrawMenu(GameTime gameTime)
+        {
+            game.gui_manager.Draw(gameTime);
+        }
+
+        private void CreateMenuControls(Screen mainScreen)
+        {
+            //TODO: Change Label Colors to white
+            //Account Name Label
+            LabelControl accountNameLabel = new LabelControl();
+            accountNameLabel.Text = "Account Name";
+            accountNameLabel.Bounds = new UniRectangle(200.0f, 150.0f, 110.0f, 24.0f);
+            mainScreen.Desktop.Children.Add(accountNameLabel);
+
+            //Account Name Input
+            InputControl accountNameInput = new InputControl();
+            accountNameInput.Bounds = new UniRectangle(200.0f, 175.0f, 200.0f, 24.0f);
+            accountNameInput.Text = "";
+            mainScreen.Desktop.Children.Add(accountNameInput);
+
+            //Password Label
+            LabelControl passwordLabel = new LabelControl();
+            passwordLabel.Text = "Password";
+            passwordLabel.Bounds = new UniRectangle(200.0f, 200.0f, 110.0f, 24.0f);
+            mainScreen.Desktop.Children.Add(passwordLabel);
+
+            //TODO: Create Password field where characters show up as black circles
+            //Password Input
+            InputControl passwordInput = new InputControl();
+            passwordInput.Bounds = new UniRectangle(200.0f, 225.0f, 200.0f, 24.0f);
+            mainScreen.Desktop.Children.Add(passwordInput);
+
+            //TODO: Change to a contrasting color (Yellow)
+            //Login Button.
+            ButtonControl loginButton = GuiHelper.CreateButton("Login", -415, -50, 150, 32);
+            loginButton.Pressed += delegate(object sender, EventArgs arguments)
+            {
+                game.EnterMainMenu();
+            };
+            mainScreen.Desktop.Children.Add(loginButton);
+
+            //Debug Button.
+            ButtonControl multiplayerButton = GuiHelper.CreateButton("MULTIPLAYER", -400, -300, 200, 32);
+            multiplayerButton.Pressed += delegate(object sender, EventArgs arguments)
+            {
+            };
+            mainScreen.Desktop.Children.Add(multiplayerButton);
+
+            //Button to close game.
+            ButtonControl quitButton = GuiHelper.CreateButton("Quit", -15, -15, 80, 32);
+            quitButton.Pressed += delegate(object sender, EventArgs arguments)
+            {
+                game.Exit();
+            };
+            mainScreen.Desktop.Children.Add(quitButton);
+        }
+    }
+}
